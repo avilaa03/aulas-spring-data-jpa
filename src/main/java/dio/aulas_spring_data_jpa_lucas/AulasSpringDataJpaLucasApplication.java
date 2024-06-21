d3039164e08ffbb2e10a797bc5e1ae575d790cd3
avilaa03 <lucasdeavila2003@gmail.com>
2024-06-21 15:40:27 -0300

    first commit

---
+++ src/main/java/dio/aulas_spring_data_jpa_lucas/AulasSpringDataJpaLucasApplication.java
@@ -0,0 +1,13 @@
+package dio.aulas_spring_data_jpa_lucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasSpringDataJpaLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasSpringDataJpaLucasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/aulas_spring_data_jpa_lucas/AulasSpringDataJpaLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.aulas_spring_data_jpa_lucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasSpringDataJpaLucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
